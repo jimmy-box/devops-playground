e5fb927e3c5300435bdf3c34a20a2a1aedb0470e
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 11:59:11 +0000

    added resources

--- myproject/deploy.sh
+++ myproject/deploy.sh
@@ -12,8 +12,9 @@ sudo apt update -y
 
 
 #install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-compose
 docker-compose --version
 
 
