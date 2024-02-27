10603ab35160208100ec63f3d3326a98e28e9f8c
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 13:22:22 +0000

    added resources

--- myproject/deploy.sh
+++ myproject/deploy.sh
@@ -18,7 +18,7 @@ sudo apt install nginx -y
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install docker-compose
-docker-compose --version
+#docker-compose --version
 
 
 
