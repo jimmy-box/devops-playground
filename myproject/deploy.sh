799c78cc3be4fa3631aa69a13e8c6ea59a7d6151
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 11:50:06 +0000

    added resources

--- myproject/deploy.sh
+++ myproject/deploy.sh
@@ -1,10 +1,22 @@
-!#/bin/bash
+#!/bin/bash
 
 set -e
 
-apt  install docker.io
-apt  install docker-compose
-docker-compose up -d
+#Install dependencies
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
+sudo apt install docker-ce
+
+
+#install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
 
 
 
