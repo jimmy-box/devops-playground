37707b5db2a13efad75110f9ff72f5a2b64abff0
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 15:31:04 +0000

    added some more commits

--- myproject/deploy.sh
+++ myproject/deploy.sh
@@ -7,6 +7,9 @@ sudo apt update -y
 sudo apt install nginx -y
 sudo apt install docker-compose
 sudo apt-get install docker-compose-plugin -y
+echo $USER
+sudo groupadd docker
+sudo usermod -aG docker $USER
 # sudo su - 
 # cd /home/ubuntu/myproject
 
