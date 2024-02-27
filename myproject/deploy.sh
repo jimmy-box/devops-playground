32d7d6c76992cc3d0b1d6ef0d3fae91208ce6dbb
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 15:08:21 +0000

    latest branch commit

---
+++ myproject/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+#Install dependencies
+sudo apt update -y
+sudo apt install nginx -y
+sudo apt install docker-compose
+sudo apt-get install docker-compose-plugin -y
+# sudo su - 
+# cd /home/ubuntu/myproject
+
+
+
+
+
+# sudo docker compose up -d
+
