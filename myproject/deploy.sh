48dce70018e01043edd723efac4ee20460382163
Jamesie1234 <james.adesanlu@news.co.uk>
2024-02-27 12:33:32 +0000

    added resources

--- myproject/deploy.sh
+++ myproject/deploy.sh
@@ -9,6 +9,7 @@ sudo apt update -y
 # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 # #apt-cache policy docker-ce
 # sudo apt install docker-ce
+#sudo systemctl status docker
 
 
 #install docker compose
